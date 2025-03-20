61b01cb69ca6ff4d1cfafb04d8a783f457b2b57f
Luccas <luccastabac@gmail.com>
2025-03-20 12:29:40 -0300

    .

--- aula.py
+++
@@ -1,9 +0,0 @@
-nome = gabriel
-peso = 52
-altura = 1.67
-casado = false 
-
-print (type(nome))
-print (type(peso))
-print(type(altura))
-print(type(casado))
--- nome de uma pessoa.py
+++
@@ -1,3 +0,0 @@
-nome = gabriel
-
-print("Bem vindo ao mundo da programação", nome)
