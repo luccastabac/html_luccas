d6d641b3817a01f971f1bcf5bd53a30f939e3a1d
Seu Nome <seuemail@domino.com>
2025-06-12 11:05:47 -0300

    avaliação

---
+++ conta_bancaria.js
@@ -0,0 +1,18 @@
+class ContaBancaria{
+    saldo
+    emprestimos
+    gastos
+    contas
+    receba
+
+
+    constructor(saldo, emprestimos, gastos, contas, receba){       
+        
+        this.saldo= saldo
+        this.emprestimo= emprestimos
+        this.gastos= gastos
+        this.contas= contas
+        this.receba= receba
+        
+    }
+}
